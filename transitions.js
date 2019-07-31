91ccbd79508dce814772835f1267c70e3258c002
aar026 <driean96@gmail.com>
2019-07-31 10:23:07 -0700

    Update transitions.js

--- transitions.js
+++ transitions.js
@@ -60,20 +60,6 @@ Barba.Pjax.getTransition = function() {
 
   return FadeTransition;
 };
-Barba.Dispatcher.on('newPageReady', function (currentStatus, oldStatus, container) {
-    $(container).find('script').each(function (i, script) {
-        var $script = $(script);
-        $.ajax({
-            url: $script.attr('src'),
-            cache: true,
-            dataType: 'script',
-            success: function () {
-                $script.trigger('load');
-            }
-        });
-    });
-});
-
 Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
     eval(container.querySelector("script").innerHTML);
 });
